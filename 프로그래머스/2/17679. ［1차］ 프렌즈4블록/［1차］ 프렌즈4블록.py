45b2317a69c3a56ccf1aaf30eba65d323630e0ed
parkseonmin <89953212+psm1109@users.noreply.github.com>
2025-07-29 11:27:06 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 57.42 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
@@ -3,23 +3,6 @@ def solution(m, n, board):
     for i in range(m):
         board[i] = list(board[i])
     
-    def check(y,x):
-        s = set()
-        s.add((y,x))
-        while s:
-            Y,X = s.pop()
-            if Y >= m-1 or X >= n-1:
-                continue
-                
-            if board[y][x] == board[Y+1][X] == board[Y][X+1] == board[Y+1][X+1]:
-                s.add((Y+1,X))
-                s.add((Y,X+1))
-                s.add((Y+1,X+1))
-                remove_block.add((Y,X)) 
-                remove_block.add((Y+1,X)) 
-                remove_block.add((Y,X+1)) 
-                remove_block.add((Y+1,X+1)) 
-    
     def gravity(board):
         for x in range(n):
             stack = []
@@ -36,15 +19,19 @@ def solution(m, n, board):
         
     while 1:
         cnt = 0
+        remove_block = set()
         for i in range(m-1):
             for j in range(n-1):
                 if board[i][j] == "" : 
                     continue
-                remove_block = set()
-                check(i,j) #제거할 블럭 확인
-                for y,x in remove_block: #블럭 제거
-                    board[y][x] = ""
-                    cnt += 1
+                
+                if board[i][j] == board[i+1][j] == board[i][j+1] == board[i+1][j+1]:
+                    remove_block.update({(i,j),(i+1,j),(i,j+1),(i+1,j+1)})
+            
+        for y,x in remove_block: #블럭 제거
+            board[y][x] = ""
+            cnt += 1
+            
         if cnt == 0:
             break
         answer += cnt
