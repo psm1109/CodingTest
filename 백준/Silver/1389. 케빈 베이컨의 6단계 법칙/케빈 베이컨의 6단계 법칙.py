a9daa4997c08491c1d23f6d987ddf285e23ff51c
parkseonmin <89953212+psm1109@users.noreply.github.com>
2025-08-23 05:04:03 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 384 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -0,0 +1,16 @@
+N,M =map(int,input().split())
+
+graph = [[float('inf')]*N for _ in range(N)]
+
+for _ in range(M):
+    A,B = map(int,input().split())
+    graph[A-1][B-1] = 1
+    graph[B-1][A-1] = 1
+
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            graph[i][j] = min(graph[i][j], graph[i][k]+graph[k][j])
+                
+graph = [sum(g) for g in graph]
+print(graph.index(min(graph))+1)
